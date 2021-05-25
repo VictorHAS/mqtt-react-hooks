cc4519fd66529b58c664904f522331238602e241
gstvg <28798827+gstvg@users.noreply.github.com>
2021-05-25 13:20:31 -0300

    Update useSubscription.tsx

--- lib/useSubscription.tsx
+++ lib/useSubscription.tsx
@@ -9,17 +9,29 @@ export default function useSubscription(
   topic: string | string[],
   options: IClientSubscribeOptions = {} as IClientSubscribeOptions,
 ): IUseSubscription {
-  const { client, connectionStatus, message } = useContext<Context>(
+  const { client, connectionStatus } = useContext<Context>(
     MqttContext,
   );
-
+  
+  const [message, setMessage] = useState()
+ 
   const subscribe = useCallback(async () => {
-    client?.subscribe(topic, options);
+    client?.subscribe(topic, options);    
   }, [client, options, topic]);
 
   useEffect(() => {
     if (client?.connected) {
       subscribe();
+      
+      const callback = (receivedTopic, message) => {
+        if (receivedTopic === topic) {
+          setMessage({topic, message: message.toString()})
+        }
+      };
+      
+      client.on('message', callback);
+      
+      return () => client.off(callback)
     }
   }, [client, subscribe]);
 
