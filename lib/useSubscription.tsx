6c0d833917086558ff98c394b34caeddaf997b6a
gstvg <28798827+gstvg@users.noreply.github.com>
2021-09-03 08:30:01 -0300

    Make useSubscription return only the subscribed topic (#18)
    * Update useSubscription.tsx
    
    * Update Connector.tsx
    
    * Update types.ts
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useMqttState.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx
    
    * Update useSubscription.tsx

--- lib/useSubscription.tsx
+++ lib/useSubscription.tsx
@@ -1,26 +1,40 @@
-import { useContext, useEffect, useCallback } from 'react';
+import { useContext, useEffect, useCallback, useState } from 'react';
+import { matches } from 'mqtt-pattern';
 
 import { IClientSubscribeOptions } from 'mqtt';
 
 import MqttContext from './Context';
-import { IMqttContext as Context, IUseSubscription } from './types';
+import { IMqttContext as Context, IUseSubscription, IMessage } from './types';
 
 export default function useSubscription(
   topic: string | string[],
   options: IClientSubscribeOptions = {} as IClientSubscribeOptions,
 ): IUseSubscription {
-  const { client, connectionStatus, message } = useContext<Context>(
+  const { client, connectionStatus, parserMethod } = useContext<Context>(
     MqttContext,
   );
-
+  
+  const [message, setMessage] = useState<IMessage | undefined>(undefined)
+ 
   const subscribe = useCallback(async () => {
-    client?.subscribe(topic, options);
+    client?.subscribe(topic, options);    
   }, [client, options, topic]);
 
   useEffect(() => {
     if (client?.connected) {
       subscribe();
+      
+      const callback = (receivedTopic :string, message) => {
+        if ([topic].flat().some(receivedTopic => matches(topic, receivedTopic))) {
+          setMessage({topic: receivedTopic, message: parserMethod?.(message) || message.toString()})
+        }
+      };
+      
+      client.on('message', callback);
+      
+      return () => client.off('message', callback)
     }
+    return
   }, [client, subscribe]);
 
   return {
